898f19039b5e1ebe15155d93320d108fb48a83ac
freddyvidigal <freddyvidigalads@gmail.com>
2025-07-23 00:53:36 -0300

    criando pas routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-
 import './Styles/theme.css';
 import './Styles/global.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainerProps } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import { MainRouter } from './routers/MainRouter';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainerProps>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about-pomodoro' element={<AboutPomodoro />}></Route>
-
-            <Route path='*' element={<NotFound />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainerProps>
     </TaskContextProvider>
   );
